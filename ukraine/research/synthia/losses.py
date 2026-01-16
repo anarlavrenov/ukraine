215f0f243fb4eab1ca7be2a0f2509455557a13bc
anarlavrenov <anarstanislavlavrenov@gmail.com>
2026-01-16 20:04:53 +0200

    [+] added experimental.py

--- ukraine/research/synthia/losses.py
+++ ukraine/research/synthia/losses.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
 
 class GuidedAttentionLoss(nn.Module):
     def __init__(
@@ -135,114 +134,6 @@ class GuidedAttentionLoss(nn.Module):
         return output_masks.unsqueeze(-1) & input_masks.unsqueeze(-2)
 
 
-class MultiResolutionSTFTLoss(torch.nn.Module):
-    def __init__(
-            self,
-            resolutions,
-            vocoder,
-            factor_sc,
-            factor_mag
-    ):
-        super(MultiResolutionSTFTLoss, self).__init__()
-
-        self.resolutions = resolutions
-        self.vocoder = vocoder
-        self.factor_sc = factor_sc
-        self.factor_mag = factor_mag
-        self.epsilon = torch.finfo(torch.float32).eps
-
-        self.vocoder.eval()
-        for p in self.vocoder.parameters():
-            p.requires_grad = False
-
-        self._windows = {}
-
-    def _window(self, win_length, x: torch.Tensor):
-        key = (win_length, x.device, x.dtype)
-        if key not in self._windows:
-            self._windows[key] = torch.hann_window(win_length, device=x.device, dtype=x.dtype)
-        return self._windows[key]
-
-    def spectral_convergence_loss(
-            self,
-            pred_mag: torch.Tensor,
-            true_mag: torch.Tensor
-    ):
-        return torch.norm(true_mag - pred_mag, p="fro") / (torch.norm(true_mag, p="fro") + self.epsilon)
-
-    def log_stft_magnitude_loss(
-            self,
-            pred_mag,
-            true_mag
-    ):
-        return F.l1_loss(torch.log(self.epsilon + true_mag), torch.log(self.epsilon + pred_mag))
-
-    def forward(
-            self, batch_pred_mel: torch.Tensor,
-            batch_true_mel: torch.Tensor,
-            tgt_key_padding_mask: torch.Tensor
-    ) -> float:
-
-        B, T, M = batch_pred_mel.shape
-        # B
-        mel_lens = (~tgt_key_padding_mask).sum(dim=1)
-
-        sc_loss = torch.tensor(0.0, device=batch_pred_mel.device)
-        mag_loss = torch.tensor(0.0, device=batch_pred_mel.device)
-        valid_count = 0
-
-        for i in range(B):
-            Lm = int(mel_lens[i].item())
-            if Lm <= 0:
-                continue
-
-            valid_count += 1
-
-            # B, M, T
-            pred_mel_i = batch_pred_mel[i:i + 1, :Lm, :].transpose(2, 1).float()
-            true_mel_i = batch_true_mel[i:i + 1, :Lm, :].transpose(2, 1).float()
-
-
-            # B. 1, T --> B, T
-            pred_wav_i = self.vocoder(pred_mel_i).squeeze(1)
-            with torch.no_grad():
-                true_wav_i = self.vocoder(true_mel_i).squeeze(1)
-
-            pred_wav_i = pred_wav_i.to(torch.float32)
-            true_wav_i = true_wav_i.to(torch.float32)
-
-            sample_sc_loss = 0
-            sample_mag_loss = 0
-
-            for (n_fft, win_length, hop_length) in self.resolutions:
-                window = self._window(win_length, pred_wav_i)
-
-                pred_stft = torch.stft(
-                    pred_wav_i, n_fft, hop_length, win_length,
-                    window=window, return_complex=True
-                )
-
-                true_stft = torch.stft(
-                    true_wav_i, n_fft, hop_length, win_length,
-                    window=window, return_complex=True
-                )
-
-                pred_mag = torch.abs(pred_stft)
-                true_mag = torch.abs(true_stft)
-
-                sample_sc_loss += self.spectral_convergence_loss(pred_mag, true_mag)
-                sample_mag_loss += self.log_stft_magnitude_loss(pred_mag, true_mag)
-
-            sc_loss += sample_sc_loss / len(self.resolutions)
-            mag_loss += sample_mag_loss / len(self.resolutions)
-
-        if valid_count > 0:
-            sc_loss = sc_loss / valid_count
-            mag_loss = mag_loss / valid_count
-
-        return self.factor_sc * sc_loss + self.factor_mag * mag_loss
-
-
 class SynthiaLoss(nn.Module):
   def __init__(
           self,
@@ -251,9 +142,7 @@ class SynthiaLoss(nn.Module):
           ga_n_layer,
           ga_n_head_start,
           reduction_factor,
-          pos_weight,
-          mrstft_loss = None,
-          mrstft_weight = None
+          pos_weight
   ):
       super().__init__()
 
@@ -269,9 +158,6 @@ class SynthiaLoss(nn.Module):
           pos_weight=pos_weight,
           reduction="none"
       )
-
-      self.mrstft_loss = mrstft_loss
-      self.mrstft_weight = 0.0 if mrstft_weight is None else mrstft_weight
 
   def forward(
           self,
@@ -302,17 +188,12 @@ class SynthiaLoss(nn.Module):
       stop_loss = self.bce_criterion(stop_pred, stop_true)
       stop_loss = (stop_loss * valid_mask_bce).sum() / (valid_mask_bce.sum() + 1e-8)
 
-      mrstft_loss = torch.tensor(0.0, device=mel_final.device)
-      if self.mrstft_loss is not None:
-        mrstft_loss = self.mrstft_loss(mel_final, mel_true, tgt_key_padding_mask)
-
 
       return (
           mel_base_loss,
           mel_final_loss,
           guided_attention_loss,
-          stop_loss,
-          mrstft_loss * self.mrstft_weight
+          stop_loss
       )
 
   @staticmethod
