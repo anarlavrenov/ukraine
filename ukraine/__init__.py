49e41b88f68f1b068673c0c76dad48d178cc46f0
anarlavrenov <anarstanislavlavrenov@gmail.com>
2026-01-16 19:52:33 +0200

    [+] added experimental.py

--- ukraine/__init__.py
+++ ukraine/__init__.py
@@ -1,4 +1,4 @@
 """
 Ukraine: A Data Science Toolkit
 """
-__version__ = "0.3.3"
+__version__ = "0.3.4"
--- ukraine/research/synthia/losses.py
+++ ukraine/research/synthia/losses.py
@@ -199,8 +199,8 @@ class MultiResolutionSTFTLoss(torch.nn.Module):
             valid_count += 1
 
             # B, M, T
-            pred_mel_i = batch_pred_mel[i:i + 1, :Lm, :].transpose(2, 1)
-            true_mel_i = batch_true_mel[i:i + 1, :Lm, :].transpose(2, 1)
+            pred_mel_i = batch_pred_mel[i:i + 1, :Lm, :].transpose(2, 1).float()
+            true_mel_i = batch_true_mel[i:i + 1, :Lm, :].transpose(2, 1).float()
 
 
             # B. 1, T --> B, T
